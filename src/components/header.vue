db20f9d995b7fb3f617d8041f87df3d57bd4ac68
ayurev87 <a-yurev_87@mail.ru>
2024-02-02 01:33:31 +0300

    add 1create vue

---
+++ src/components/header.vue
@@ -0,0 +1,19 @@
+<template>
+   <header class="header">
+    <div class="container">
+        Привет
+    </div>
+   </header>
+</template>
+
+<script>
+    
+    export default {
+    name: 'headerBlock',
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import '@/scss/fonts.scss'
+import '@/scss/_vars.scss'
+import '@/scss/style.scss'
+import '@/scss/svg.scss'
 
 createApp(App).use(store).mount('#app')
