9e4340ccc2e2fe4911b55f6d66bfff6038e1a66c
ayurev87 <a-yurev_87@mail.ru>
2024-02-09 00:51:32 +0300

    add

--- vue.config.js
+++ vue.config.js
@@ -13,3 +13,8 @@ module.exports = defineConfig({
     ],
   },
 })
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    : '/'
+}
